f5d9036871c8d2ac875b2cc0a01e57fecc4f839c
Thales Renan <thalesrmn@hotmail.com>
2023-09-13 23:09:18 -0300

    FIX: gera um novo número somente após acertar o anterior

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,35 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
 
   const [guess, setGuess] = useState("")
+  const [lastGuess, setLastGuess] = useState(0)
+  const [randomNumber, setRandomNumber] = useState(0)
+  const [lastRandomNumber, setLastRandomNumber] = useState(0)
   const [innerText, setInnerText] = useState("Digite um número de 1 a 10");
- 
-  const randomNumber = Math.floor(Math.random() * 10) + 1;
 
-  const checkNumber = () => {
-    if (randomNumber === parseInt(guess)) {
-      setInnerText("Você acertou!!")
-    } else if (parseInt(guess) > randomNumber) {
-      setInnerText("Seu chute foi maior. Tente novamente.")
-    } else if (parseInt(guess) < randomNumber) {
-      setInnerText("Seu chute foi menor. Tente novamente.")
-    } else {
-      setInnerText("Digite um número de 1 a 10.")
+    function calculateRandomNumber() { 
+      setRandomNumber(Math.floor(Math.random() * 10) + 1)
     }
-  }
+  
+  useEffect(() => {
+    calculateRandomNumber();
+  },[])
+  
+    const checkNumber = () => {
+      if (randomNumber === parseInt(guess)) {
+        setInnerText("Você acertou!!")
+        setLastRandomNumber(randomNumber)
+        calculateRandomNumber()
+      } else if (parseInt(guess) > randomNumber) {
+        setInnerText("Seu chute foi maior. Tente novamente.")
+      } else if (parseInt(guess) < randomNumber) {
+        setInnerText("Seu chute foi menor. Tente novamente.")
+      } else {
+        setInnerText("Digite um número de 1 a 10.")
+      }
+    }
 
     return (
       <>
@@ -34,13 +45,14 @@ export default function Home() {
         min-h-screen
         bg-gradient-to-b from-slate-800 to-slate-950
         ">
+         
           <h1 className="text-slate-200 text-2xl">{innerText}</h1>
           <form
             onSubmit={(e) => {
               e.preventDefault()
               checkNumber()
               setGuess("")
-              console.log("Your guess: " + guess, " || Random Number: " + randomNumber)
+              setLastGuess(parseInt(guess))
             }}>
             <input
               value={guess}
@@ -65,10 +77,15 @@ export default function Home() {
           rounded-2xl 
           p-2 
           mt-5
+          mb-10
           bg-slate-200
           ">
             Adivinhe
           </button>
+          <div>
+          <p className="text-slate-200">Último chute: {lastGuess}</p>
+          <p className="text-slate-200">Último número aleatório: {lastRandomNumber}</p>
+          </div>
         </main>
       </>
     );
